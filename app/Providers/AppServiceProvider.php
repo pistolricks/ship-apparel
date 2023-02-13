170b1e0152b841ab82ce4f969e2d9fa9282824fd
lukinovec <lukinovec@gmail.com>
2023-02-13 11:48:07 +0100

    Make Cashier use the Tenant model (#127)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\Tenant;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Cashier\Cashier;
 
@@ -25,5 +26,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Cashier::ignoreMigrations();
+        Cashier::useCustomerModel(Tenant::class);
     }
 }
