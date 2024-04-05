93bc102fa43667f3460200228fc30f925db57fd9
Samuel Štancl <samuel.stancl@gmail.com>
2024-04-05 23:08:27 +0200

    fix #148

--- app/PloiManager.php
+++ app/PloiManager.php
@@ -37,7 +37,7 @@ class PloiManager
             return;
         }
 
-        Http::withToken($this->token)->asJson()
+        Http::withToken($this->token)->asJson()->acceptJson()
             ->post("https://ploi.io/api/servers/{$this->server}/sites/{$this->site}/tenants", [
                 'tenants' => [$domain->domain],
             ]
@@ -53,7 +53,7 @@ class PloiManager
             return;
         }
 
-        Http::withToken($this->token)->asJson()
+        Http::withToken($this->token)->asJson()->acceptJson()
             ->delete("https://ploi.io/api/servers/{$this->server}/sites/{$this->site}/tenants/$domain->domain");
     }
 
@@ -66,7 +66,7 @@ class PloiManager
             return;
         }
 
-        Http::withToken($this->token)->asJson()
+        Http::withToken($this->token)->asJson()->acceptJson()
             ->post("https://ploi.io/api/servers/{$this->server}/sites/{$this->site}/tenants/{$domain->domain}/request-certificate", [
                 'webhook' => tenant()->route('tenant.ploi.certificate.issued'),
             ]
@@ -84,7 +84,7 @@ class PloiManager
             return;
         }
 
-        Http::withToken($this->token)->asJson()
+        Http::withToken($this->token)->asJson()->acceptJson()
             ->post("https://ploi.io/api/servers/{$this->server}/sites/{$this->site}/tenants/{$domain->domain}/revoke-certificate", [
                 'webhook' => tenant()->route('tenant.ploi.certificate.revoked'),
             ]
@@ -102,7 +102,7 @@ class PloiManager
             return;
         }
 
-        Http::withToken($this->token)->asJson()
+        Http::withToken($this->token)->asJson()->acceptJson()
             ->post("https://ploi.io/api/servers/{$this->server}/databases/acknowledge", [
                 'name' => $databaseName,
                 'description' => 'Tenant database',
@@ -121,7 +121,7 @@ class PloiManager
             return;
         }
 
-        Http::withToken($this->token)->asJson()
+        Http::withToken($this->token)->asJson()->acceptJson()
             ->delete("https://ploi.io/api/servers/{$this->server}/databases/$databaseName/forget");
     }
 }
