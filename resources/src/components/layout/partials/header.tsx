7efad68cb4905f894aff93a01efad4a9ca049491
Ollivr <66511791+ollivr@users.noreply.github.com>
2025-04-14 06:19:07 -0700

    added

--- resources/src/components/layout/partials/header.tsx
+++ resources/src/components/layout/partials/header.tsx
@@ -16,9 +16,7 @@ const Header: Component<{
 
     return (
         <>
-            <div class={'h-8 mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 items-center flex font-light text-xs'}>
 
-            </div>
             <header class="h-16 sm:h-20">
                 <nav class={''} aria-label="Top">
                     <div class="bg-primary/10 backdrop-blur-md backdrop-filter h-16 sm:h-20">
