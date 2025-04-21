63f1d0770ec42a5615e571c2671b2b44eb292f95
Ollivr <66511791+ollivr@users.noreply.github.com>
2025-04-21 16:18:30 -0700

    added

--- resources/src/components/custom-apparel/hero.tsx
+++ resources/src/components/custom-apparel/hero.tsx
@@ -1,17 +1,115 @@
 import {Component} from "solid-js";
 import {ScrollingCards} from "~/components/section/category/scrolling-cards";
+import categories from '~/lib/custom-apparel-menus.json'
 
 type PROPS = {}
 
 const Hero: Component<PROPS> = props => {
 
-    const categories = '~/lib/custom-apparel-menus.json'
 
     return (
         <div>
-            <ScrollingCards categories={JSON.parse(categories)}/>
+            <ScrollingCards categories={one}/>
+            <ScrollingCards categories={second}/>
+            <ScrollingCards categories={third}/>
         </div>
     );
 };
 
 export default Hero;
+
+
+const one = [
+    {
+        "name": "T-Shirts",
+        "src": "http://localhost:8080/insecure/rs:fill:320:224:0/g:sm/plain/https://ink-and-thread.com/storage/assets/stacked/PC54_flat_front_GA16.png",
+        "href": "/",
+        "width": 240,
+        "height": 320
+    },
+    {
+        "name": "Hoodies",
+        "src": "http://localhost:8080/insecure/rs:fill:224:320:0/g:sm/plain/https://ink-and-thread.com/storage/assets/stacked/TM1MW450_multiple_stack_GA22.png",
+        "href": "/",
+        "width": 320,
+        "height": 320
+    },
+    {
+        "name": "Hats",
+        "src": "http://localhost:8080/insecure/rs:fill:224:320:0/g:sm/plain/https://ink-and-thread.com/storage/assets/stacked/CP82_stack.png",
+        "href": "/",
+        "width": 223,
+        "height": 320
+    },
+    {
+        "name": "Pants & Shorts",
+        "src": "http://localhost:8080/insecure/rs:fill:300:400:0/g:sm/plain/https://ink-and-thread.com/storage/assets/stacked/DT6107_multiple_flats_front_GA21.png",
+        "href": "/",
+        "width": 224,
+        "height": 320
+    },
+]
+
+export const second = [
+    {
+        "name": "Beanies",
+        "src": "http://localhost:8080/insecure/rs:fill:300:400:0/g:sm/plain/https://ink-and-thread.com/storage/assets/stacked/NF0A4VUB_pair_GA21.png",
+        "href": "/",
+        "width": 224,
+        "height": 320
+    },
+    {
+        "name": "Jackets",
+        "src": "http://localhost:8080/insecure/rs:fill:300:400:0/g:sm/plain/https://ink-and-thread.com/storage/assets/stacked/578674_anthhthrblackgrey_flat_stack_ga19.png",
+        "href": "/",
+        "width": 240,
+        "height": 320
+    },
+    {
+        "name": "Polo Shirts",
+        "src": "http://localhost:8080/insecure/rs:fill:300:400:0/g:sm/plain/https://ink-and-thread.com/storage/assets/stacked/K572_flat_GA18.png",
+        "href": "/",
+        "width": 350,
+        "height": 320
+    },
+
+    {
+        "name": "Women's",
+        "src": "http://localhost:8080/insecure/rs:fill:300:400:0/g:sm/plain/https://ink-and-thread.com/storage/assets/stacked/MM3023_allcolors_lifestyle_FA22.png",
+        "href": "/",
+        "width": 224,
+        "height": 320
+    }
+]
+
+export const third = [
+    {
+        "name": "Activewear",
+        "src": "http://localhost:8080/insecure/rs:fill:300:400:0/g:sm/plain/https://ink-and-thread.com/storage/assets/stacked/NKBQ5231_All_Stacked_ga19.png",
+        "href": "/",
+        "width": 350,
+        "height": 320
+    },
+    {
+        "name": "Business Apparel",
+        "src": "http://localhost:8080/insecure/rs:fill:300:400:0/g:sm/plain/https://ink-and-thread.com/storage/assets/stacked/S658_hanging_GA18.png",
+        "href": "/",
+        "width": 224,
+        "height": 320
+    },
+    {
+        "name": "Workwear",
+        "src": "http://localhost:8080/insecure/rs:fill:300:400:0/g:sm/plain/https://ink-and-thread.com/storage/assets/stacked/CS200_Stack_FA21.png",
+        "href": "/",
+        "width": 224,
+        "height": 320
+    },
+    {
+        "name": "Featured Brands",
+        "src": "http://localhost:8080/insecure/rs:fill:300:400:0/g:sm/plain/https://ink-and-thread.com/storage/assets/stacked/TM1MW450_multiple_stack_GA22.png",
+        "href": "/",
+        "width": 224,
+        "height": 320
+    },
+]
+
--- resources/src/components/section/category/scrolling-cards.tsx
+++ resources/src/components/section/category/scrolling-cards.tsx
@@ -1,25 +1,31 @@
 import {Component, For} from "solid-js";
 import { MenuItemType } from "~/lib/types";
+import {A} from "@solidjs/router";
 
 
 
 
 const ScrollingCards: Component<{
-    categories: MenuItemType[]
+    categories: {name: string, href: string, src: string, width: number, height: number}[]
 }> = props => {
     const categories = () => props.categories;
 
+
     return (
         <div class="bg-white">
-            <div class="py-16 sm:py-24 xl:mx-auto xl:max-w-7xl xl:px-8">
+            <div class=" xl:mx-auto xl:max-w-7xl ">
                 <div class="mt-4 flow-root">
                     <div class="-my-2">
-                        <div class="relative box-content h-80 overflow-x-auto py-2 xl:overflow-visible">
+                        <div class="relative box-content h-80 overflow-hidden py-2 xl:overflow-visible">
                             <div
-                                class="absolute flex space-x-8 px-4 sm:px-6 lg:px-8 xl:relative xl:grid xl:grid-cols-5 xl:gap-x-8 xl:space-x-0 xl:px-0">
+                                class="absolute flex space-x-8 px-4 xl:relative xl:grid xl:grid-cols-5 xl:gap-x-8 xl:space-x-0 xl:px-0">
                                 <For each={categories()}>
                                     {(category) => (
-                                        <a
+                                        <A
+                                            style={{
+                                                width: `${category.width}px`,
+                                                height: `${category.height}px`,
+                                            }}
                                             href={category.href}
                                             class="relative flex h-80 w-56 flex-col overflow-hidden rounded-lg p-6 hover:opacity-75 xl:w-auto"
                                         >
@@ -31,8 +37,8 @@ const ScrollingCards: Component<{
                                                 class="absolute inset-x-0 bottom-0 h-2/3 bg-gradient-to-t from-gray-800 opacity-50"
                                             />
                                             <span
-                                                class="relative mt-auto text-center text-xl font-bold text-white">{category.title}</span>
-                                        </a>
+                                                class="relative mt-auto text-center text-xl font-bold text-white">{category.name}</span>
+                                        </A>
                                     )}
                                 </For>
                             </div>
