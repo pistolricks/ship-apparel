73387c662aa50e6255ab239f199b360ae155c5be
Ollivr <66511791+ollivr@users.noreply.github.com>
2025-04-21 20:37:54 -0700

    added

--- resources/src/components/layout/partials/navigation.tsx
+++ resources/src/components/layout/partials/navigation.tsx
@@ -44,7 +44,7 @@ export default function Navigation(props: { contextId: string }) {
                 <nav aria-label="Top">
 
                     <div class="bg-sky-400/70">
-                        <div class="mx-auto flex h-10 max-w-7xl items-center justify-between px-2">
+                        <div class="mx-auto flex h-10 max-w-7xl items-center justify-between">
 
                             <div class="hidden lg:block lg:flex-1">
 
@@ -173,8 +173,6 @@ export default function Navigation(props: { contextId: string }) {
                                                             <path stroke-linecap="round" stroke-linejoin="round"
                                                                   d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"/>
                                                         </svg>
-                                                        <span
-                                                            class="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">0</span>
                                                         <span class="sr-only">items in cart, view bag</span>
                                                     </Drawer.Trigger>
 
