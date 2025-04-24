04261c96435f72be09384dca1c664b218807a348
Ollivr <66511791+ollivr@users.noreply.github.com>
2025-04-23 23:47:59 -0700

    ts-ignore on @lucide.d.ts

--- resources/src/@types/lucide.d.ts
+++ resources/src/@types/lucide.d.ts
@@ -1,5 +1,6 @@
 declare module "lucide-solid/icons/*" {
-    import {LucideProps} from "lucide-solid";
+    // @ts-ignore
+    import { LucideProps } from "lucide-solid/dist/types/types";
     import {Component} from "solid-js";
     const cmp: Component<LucideProps>;
 
