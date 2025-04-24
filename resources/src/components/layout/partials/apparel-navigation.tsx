7c04a881187269e9319c0dd4d699b05bffd1e269
Ollivr <66511791+ollivr@users.noreply.github.com>
2025-04-23 21:36:27 -0700

    Refactor menu components for improved structure and styling
    Introduced prop updates, child handling, and layout adjustments in `menu-left-images-right` and `apparel-navigation` components. Simplified redundant logic, enhanced flexibility with dynamic children content, and improved UI consistency with updated styles and structure.

--- resources/src/components/layout/partials/apparel-navigation.tsx
+++ resources/src/components/layout/partials/apparel-navigation.tsx
@@ -1,4 +1,4 @@
-import {Component, createSelector, createSignal, For, JSX, Match, Show, Switch} from "solid-js"
+import {Component, createSelector, createSignal, For, Match, Show, Switch} from "solid-js"
 
 import type {Orientation} from "@kobalte/core/navigation-menu"
 import {ContentItemType, MenuItemType} from "~/lib/types";
@@ -6,8 +6,7 @@ import MenuLeftImagesRight from "~/components/section/menu/menu-left-images-righ
 import {Tabs, TabsContent, TabsList, TabsTrigger} from "~/components/ui/tabs";
 import {Grid} from "~/components/ui/grid";
 import {A} from "@solidjs/router";
-import {CarouselAuto} from "~/components/ui/carousel/carousel-auto";
-
+import Icon from "~/components/ui/icon";
 
 
 type PROPS = {
@@ -32,7 +31,8 @@ const ApparelNavigation: Component<PROPS> = props => {
     };
 
     const handleDeselect = (data: string, event: Event) => {
-        setTitle(data)
+        console.log("Data:", data);
+        setTitle(() => data)
     }
 
     const isSelected = createSelector(getTitle)
@@ -40,50 +40,75 @@ const ApparelNavigation: Component<PROPS> = props => {
 
     return (
 
-        <div class="hidden sm:block relative h-[540px] w-full  items-left space-y-4">
+        <div class="block relative h-[540px] w-full  items-left space-y-4">
 
-            <Tabs class={'max-h-[540px] flex justify-start w-full shadow'} orientation={"horizontal"}>
+            <Tabs class={'absolute z-10 h-full w-full'} orientation={"horizontal"}>
+                <div class={"absolute z-40 w-full h-full flex justify-start shadow"}>
+                    <TabsList class="flex flex-col items-center text-left w-40 z-40">
+                        <For each={menu()?.[0]?.sub}>
+                            {(item: MenuItemType) => (
 
 
-                <TabsList class="flex flex-col items-center text-left">
-                    <For each={menu()?.[0]?.sub}>
-                        {(item: MenuItemType) => (
+                                <TabsTrigger on:mouseover={(event) => {
+                                    event.stopPropagation()
+                                    handleSelect(item.title)
+                                }} as={A} type="button"
+                                             href={item.href}
+                                             class={'font-medium tracking-wide'} value={getTitle()}>
+                                    {item.title}
+                                </TabsTrigger>
+                            )}
+                        </For>
+                    </TabsList>
+                    <div class={"aspect-14/10 absolute inset-y-0 right-0 z-0 flex justify-end items-center"}>
+                        <img class="w-full h-full object-cover"
+                             src={"https://ink-and-thread.com/storage/assets/4_21_lp/slider_1.png"}
+                             alt={""}/>
 
 
-                            <TabsTrigger on:mouseover={(event) => {
-                                event.stopPropagation()
-                                handleSelect(item.title)
-                            }} as={A} type="button"
-                                         href={item.href}
-                                         class={'w-40 font-medium tracking-wide'} value={getTitle()}>
-                                {item.title}
-                            </TabsTrigger>
-                        )}
-                    </For>
-                </TabsList>
+                    </div>
+                    <Grid cols={3} class="relative h-[540px] w-full overflow-x-hidden">
+                        <For each={menu()?.[0]?.sub}>
+                            {(item: MenuItemType) => (
+                                <TabsContent value={item.title} class="">
 
-                <Grid cols={3} class="relative h-[540px] w-full overflow-x-hidden">
-                    <For each={menu()?.[0]?.sub}>
-                        {(item: MenuItemType) => (
-                            <TabsContent value={item.title} class="">
-                                <Switch>
-                                    <Match when={getTitle() !== item.title}>
-                                        <div class={ "w-[75dvw] md:w-[85dvw] lg:w-[90dvw]" }>
-                                            <CarouselAuto slides={slides()} />
-                                        </div>
-                                    </Match>
-                                    <Match when={isSelected(item.title)}>
-                                        <div class="p-5">
+                                    <Switch>
+                                        <Match when={getTitle() !== item.title}>
+                                            <div class={"aspect-16/10"}>
 
-                                            <MenuLeftImagesRight title={item.title} href={item.href} list={item?.sub}/>
-                                        </div>
-                                    </Match>
-                                </Switch>
 
-                            </TabsContent>
-                        )}
-                    </For>
-                </Grid>
+                                            </div>
+                                        </Match>
+                                        <Match when={isSelected(item.title)}>
+                                            <div class="relative h-full w-full">
+                                                <MenuLeftImagesRight title={item.title}
+                                                                     href={item.href}
+                                                                     list={item?.sub}
+                                                >
+                                                    <Show when={getTitle() !== ""}>
+                                                        <TabsTrigger
+                                                            class={"w-[65dvw] h-8 flex items-center justify-center glass"}
+                                                            onClick={() => handleSelect("")} value={getTitle()}
+                                                        >
+                                                            <div class={"flex justify-between items-center pr-4"}>
+                                                                <Icon name={"CircleX"}
+                                                                      class={"size-4 text-gray-500 hover:text-red-300"}/>
+                                                                <span class={"uppercase"}>{item.title}</span>
+                                                            </div>
+                                                        </TabsTrigger>
+                                                    </Show>
+                                                </MenuLeftImagesRight>
+
+
+                                            </div>
+                                        </Match>
+                                    </Switch>
+                                </TabsContent>
+                            )}
+                        </For>
+
+                    </Grid>
+                </div>
 
             </Tabs>
 
